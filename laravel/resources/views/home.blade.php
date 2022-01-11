7f92f419b0f41d8367413be7ed88648303d30fef
Emanuele117 <emanuele.pica7@gmail.com>
2022-01-11 16:25:57 +0100

    exercise completed

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -2,5 +2,23 @@
 
 @section('content')
 
+@forelse ($movies as $movie)
+
+<div class="movie_title">
+    <h3>{{$movie->title}}</h3>
+    <div class="original_title">
+        <p>{{$movie->original_title}}</p>
+    </div>
+    <div class="nationality">
+        <p>{{ $movie->nationality }}</p>
+    </div>
+    <div class="date_and_vote">
+        <p>{{ $movie->date }} | {{ $movie->vote }}</p>
+    </div>
+</div>
+
+@empty
+   <p class="alert"> Sorry no movies available</p>
+@endforelse
 
 @endsection
